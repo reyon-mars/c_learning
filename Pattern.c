390f47a20691a65909e0ae1df0d5a54f4643a7d3
Reyon Rai <reyonrai50@gmail.com>
2024-06-07 17:19:47 +0545

    floyds triangle added

--- Pattern.c
+++ Pattern.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 
+void butterfly(int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; i++)
+		{
+			if()
+		}
+		
+	}
+	
+}
+
 void floydsTriangle(int height)
 {
-	int digit=1;
-	for (int i = 0; i < height; i++){
-		for (int j = 0; j <= i; j++){
+	int digit = 1;
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
 			printf("%d  ", digit);
 			digit++;
 		}
