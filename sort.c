482d0d90c6845314ae150428cdb69315ff3b397a
Reyon Rai <reyonrai50@gmail.com>
2024-06-17 17:13:04 +0545

    sorting algorithms implemented

--- sort.c
+++ sort.c
@@ -1,32 +1,71 @@
 #include<stdio.h>
-void sort(int array[],int arraylen);
+#include<stdlib.h>
 
-int main(void)
-{
+int*  bubble_sort ( int array[] , int arraylen );
+int*  selection_sort ( int array[], size_t arraylen );
+
+int main ( void ){
+
   int array[]= {9,8,7,6,4,5,3,2,1};
   int arraylen = sizeof(array)/sizeof(array[0]);
 
   sort(array,arraylen);
-  for(int i = 0; i<arraylen; i++) { 
-    printf("%d\n",array[i]);
+  
+  for( int i = 0; i < arraylen; i++) { 
+    printf ("%d\n", array[i] );
   }
   
  return 0;
+
 }
 
-void sort(int array[],int arraylen){
+int* bubble_sort( int array[] , int arraylen ){
 
-  for(int i=0; i < arraylen-1; i++){
+  for(int i = 0; i < arraylen-1; i++){
   
-    for(int j=0; j < (arraylen-i)-1;j++){
+    for(int j = 0; j < ( arraylen-i ) -1; j++){
 
-      if(array[j]>array[j+1]){
-      
-        
+      if( array[j] > array[j+1] ){
         int temp = array[i];
         array[i] = array[i+1];
         array[i+1]= temp;
       }
     }
   }
+  return array;
+}
+
+int*  selection_sort ( int array[], size_t arraylen){
+
+  size_t index_of_smallest;
+
+  for( size_t i = 0; i < arraylen-1; i++) {
+
+    index_of_smallest = i; 
+
+    for( size_t j = i; j < arraylen; j++) {
+      if( array[j+1] < array[j]){
+        index_of_smallest = j+1;
+      }
+    }
+  }
+  return array;
+}
+
+
+int*  insertion_sort( int array[] , size_t arraylen){
+
+  for( size_t i = 1; i < arraylen; i++) {
+    
+    int extract_position = i;
+    int value = array[ extract_position ];
+    int current_index = i;
+
+    while( current_index != 0 && array[ current_index - 1 ] > value) {
+      array[ current_index] = array[ current_index-1 ];
+      current_index--;
+    }
+    array[ current_index ] = value;
+  }
+  return array;
 }
