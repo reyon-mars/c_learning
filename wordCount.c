f231d6f38616a24b1c41ae26fadf8ed7d916031a
Reyon Rai <reyonrai50@gmail.com>
2024-08-17 18:06:55 +0545

    New Files I Guess

---
+++ wordCount.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#define YES 1
+#define NO 0
+
+int main( void )
+{
+	int c, nl, nw, nc, inword;
+	inword = NO;
+	nl = nw = nc = 0;
+	while( (c = getchar()) != EOF){
+		++nc;
+		if( c == '\n')
+			nl++;
+		
+		if( c == ' ' || c == '\n' || c == '\t' ) inword = NO;
+		else if ( inword == NO ){
+			inword = YES;
+			++nw;
+		}
+	}
+	printf(" %d %d %d ", nw, nl, nc );
+	return 0;
+}
