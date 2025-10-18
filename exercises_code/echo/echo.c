240a25a67a108b624dee4644ff9fecb12270afa4
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2025-10-18 22:01:26 +0530

    Update echo.c

--- exercises_code/echo/echo.c
+++ exercises_code/echo/echo.c
@@ -1,8 +1,10 @@
 #include <string.h>
 #include <unistd.h>
 
-int main ( int argc, char** argv){
-	for( int i = 1; i < argc; i++ ){
+int main ( int argc, char** argv)
+{
+	for( int i = 1; i < argc; i++ )
+	{
 		write( 1, argv[i], strlen(argv[i]) );
 	}
 	return 0;
