7d5461fe2515597029f9eee228400421dc23ffeb
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2025-10-18 22:02:59 +0530

    Fix parity_bit definition and format main function

--- exercises_code/register_value/register_value.c
+++ exercises_code/register_value/register_value.c
@@ -3,10 +3,13 @@
 
 static uint8_t register = 0x0;
 
-#define parity_bit  12;
+#define parity_bit  0xC;
 
-int main( void ){
+int main( void )
+{
 	uint8_t parity = register &  parity_bit;
+	parity >>= 2;
+	
 	printf( "%d", parity );
 	return 0;
 }
