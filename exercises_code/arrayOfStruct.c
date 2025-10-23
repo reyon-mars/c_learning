6aebe51bca93bac1349eb422bac74bcf880d6e3f
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2025-10-23 22:04:01 +0530

    Increase name size and add a fourth student

--- exercises_code/arrayOfStruct.c
+++ exercises_code/arrayOfStruct.c
@@ -3,7 +3,7 @@
 
 typedef struct
 {
-  char name[50];
+  char name[64];
   double gpa;
 } student;
 
@@ -12,9 +12,10 @@ int main(void)
   student student1 = {"Roni Rana", 4.00};
   student student2 = {"Reyon Rai", 3.00};
   student student3 = {"Yoo Win ", 4.00};
+  student student4 = {"Yesmin Gurung", 4.00};
 
-  student students[] = {student1, student2, student3};
-  // struct student students[]={student1,student2,student3};
+  student students[] = {student1, student2, student3, student4};
+  // struct student students[]={student1,student2,student3, student4};
 
   for (int i = 0; i < sizeof(students) / sizeof(students[0]); i++)
   {
